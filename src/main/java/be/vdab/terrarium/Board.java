77154f26f4f453af9d03c27ab1e3cc84b62af7b0
Jonas <j_busseniers@hotmail.com>
2017-09-04 13:39:37 +0200

    with loop

--- src/main/java/be/vdab/terrarium/Board.java
+++ src/main/java/be/vdab/terrarium/Board.java
@@ -1,4 +1,35 @@
 package be.vdab.terrarium;
 
-public class Board {
+import be.vdab.enteties.Herbivore;
+import be.vdab.enteties.Organism;
+
+public enum Board {
+
+	INSTANCE;
+	private Organism[][] organisms;
+
+	private Board() {
+	}
+
+	private Board(Organism[][] organisms) {
+		this.organisms = organisms;
+	}
+
+	public void nextDay() {
+		generateNewPlants();
+
+		for (int i = 0; i < organisms.length; i++) {
+			for (int j = 0; j < organisms[i].length; j++) {
+				if (organisms[i][j] instanceof Herbivore) {
+
+				}
+
+			}
+		}
+	}
+
+	private void generateNewPlants() {
+		// TODO Auto-generated method stub
+
+	}
 }
