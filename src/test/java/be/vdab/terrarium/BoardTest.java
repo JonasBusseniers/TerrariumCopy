129900105c4bb97c2aa503be6a5a80ef41aa35e6
Steven <steven.gryp@outlook.com>
2017-09-05 14:40:04 +0200

    BoardTest met inhoud

--- src/test/java/be/vdab/terrarium/BoardTest.java
+++ src/test/java/be/vdab/terrarium/BoardTest.java
@@ -2,7 +2,6 @@ package be.vdab.terrarium;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import be.vdab.entities.Carnivore;
@@ -11,7 +10,6 @@ import be.vdab.entities.Organism;
 import be.vdab.entities.Plant;
 
 public class BoardTest {
-//Ze moeten nog opgevuld
 	
 	@Test
 	public void herbivoreEatsPlantOnRight() {
@@ -19,201 +17,128 @@ public class BoardTest {
 		Herbivore herbivore = new Herbivore(3, false);
 		Organism [][]organisms = Board.getOrganisms();
 		organisms[1][2] = herbivore;
-		Board.Organisms[1][3] = plant;
+		organisms[1][3] = plant;
 		Board.eat(1,2,1,3);
-		assertEquals(null, Board.Organisms[1][3]);
-	}
-	
-	@Test
-	public void herbivoreDoesNotEatPlantOnLeft() {
-		Plant plant = new Plant(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[1][1] = plant;
-		Board.eat(1,2,1,3);
-		assertEquals(plant, Board.Organisms[1][1]);
-	}
-	
-	@Test
-	public void herbivoreDoesNotEatPlantOnTop() {
-		Plant plant = new Plant(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[0][2] = plant;
-		Board.eat(1,2,1,3);
-		assertEquals(plant, Board.Organisms[0][1]);
-	}
-	
-	@Test
-	public void herbivoreDoesNotEatPlantOnBottom() {
-		Plant plant = new Plant(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[2][2] = plant;
-		Board.eat(1,2,1,3);
-		assertEquals(plant, Board.Organisms[2][1]);
+		assertEquals(null, organisms[1][3]);
 	}
 	
 	@Test
 	public void herbivoreAfterEatingRaiseslevelWithPlantLevel() {
 		Plant plant = new Plant(1, false);
 		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[1][3] = plant;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][2] = herbivore;
+		organisms[1][3] = plant;
 		Board.eat(1,2,1,3);
-		assertEquals(4, Board.Organisms[1][2].life);
+		assertEquals(4, organisms[1][2].getLife());
 	}
 	
 	@Test
 	public void herbivoreMatesWithHerbivoreOnRight() {
-
-	}
-	
-	@Test
-	public void herbivoreDoesNotMateWithCarnivoreOnRight() {
-		
-	}
-	
-	@Test
-	public void herbivoreDoesNotMateWithHerbivoreOnLeft() {
-
-	}
-	
-	@Test
-	public void herbivoreDoesNotMateWithHerbivoreOnTop() {
-
-	}
-	
-	@Test
-	public void herbivoreDoesNotMateWithHerbivoreOnBottom() {
-
-	}
-	
-	@Test
-	public void afterMatingNewHerbivorePopsUp () {
-
+		Herbivore herbivore = new Herbivore(3, false);
+		Herbivore herbivore2 = new Herbivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		for (int i = 0; i < organisms.length; i++) {
+			for (int j = 0; j < organisms[i].length; j++) {
+				organisms[i][j] = new Plant(1,false);
+			}
+		}
+		organisms[0][0] = herbivore;
+		organisms[0][1] = herbivore2;
+		organisms[0][2] = null;
+		Board.mate();
+		assertTrue(organisms[0][2]instanceof Herbivore);	
 	}
 	
 	@Test
 	public void herbivoreMovesWhenRightIsEmpty() {
 		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[1][3] = null;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][2] = herbivore;
+		organisms[1][3] = null;
 		Board.move(1,2);
-		assertEquals(null, Board.Organisms[1][2]);
+		assertEquals(null, organisms[1][2]);
 	}
 	
 	@Test
 	public void carnivoreEatsHerbivoreOnRight() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = carnivore;
-		Board.Organisms[1][3] = herbivore;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][2] = carnivore;
+		organisms[1][3] = herbivore;
 		Board.eat(1,2,1,3);
-		assertEquals(null, Board.Organisms[1][3]);
+		assertEquals(null, organisms[1][3]);
 	}
 	
 	@Test
 	public void carnivoreDoesNotEatPlantOnRight() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Plant plant = new Plant(3, false);
-		Board.Organisms[1][2] = carnivore;
-		Board.Organisms[1][3] = plant;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][2] = carnivore;
+		organisms[1][3] = plant;
 		Board.nextDay();
-		assertEquals(plant, Board.Organisms[1][3]);
-	}
-	
-	@Test
-	public void carnivoreDoesNotEatHerbivoreOnLeft() {
-		Carnivore carnivore = new Carnivore(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][1] = herbivore;
-		Board.Organisms[1][2] = carnivore;
-		Board.eat(1,2,1,3);
-		assertEquals(herbivore, Board.Organisms[1][1]);
-	}
-	
-	@Test
-	public void carnivoreDoesNotEatHerbivoreOnTop() {
-		Carnivore carnivore = new Carnivore(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][1] = herbivore;
-		Board.Organisms[2][1] = carnivore;
-		Board.eat(1,2,1,3);
-		assertEquals(herbivore, Board.Organisms[1][1]);
-	}
-	
-	@Test
-	public void carnivoreDoesNotEatHerbivoreOnBottom() {
-		Carnivore carnivore = new Carnivore(1, false);
-		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][1] = herbivore;
-		Board.Organisms[0][1] = carnivore;
-		Board.eat(1,2,1,3);
-		assertEquals(herbivore, Board.Organisms[1][1]);
+		assertEquals(plant, organisms[1][3]);
 	}
 	
 	@Test
 	public void carnivoreAfterEatingHerbivoreRaisesLevelCarnivoreWithLevelHerbivore() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Herbivore herbivore = new Herbivore(3, false);
-		Board.Organisms[1][2] = herbivore;
-		Board.Organisms[1][1] = carnivore;
-		Board.eat(1,2,1,3);
-		assertEquals(4, Board.Organisms[1][1].life);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][2] = herbivore;
+		organisms[1][1] = carnivore;
+		Board.eat(1,1,1,2);
+		assertEquals(4, organisms[1][1].getLife());
 	}
 	
 	@Test
 	public void carnivoreFightsCarnivoreOnRight() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Carnivore carnivore2 = new Carnivore(3, false);
-		Board.Organisms[1][1] = carnivore;
-		Board.Organisms[1][2] = carnivore2;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][1] = carnivore;
+		organisms[1][2] = carnivore2;
 		Board.fight(1,1);
-		assertEquals(null, Board.Organisms[1][1]);
+		assertEquals(null, organisms[1][1]);
 	}
 	
 	@Test
 	public void whenCarnivoreFightsAndIsOnLeftThenHasActedIsFalse() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Carnivore carnivore2 = new Carnivore(3, false);
-		Board.Organisms[1][1] = carnivore;
-		Board.Organisms[1][2] = carnivore2;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[1][1] = carnivore;
+		organisms[1][2] = carnivore2;
 		Board.fight(1,1);
-		assertEquals(false, Board.Organisms[1][2].hasActed);
-	}
-	
-	@Test
-	public void carnivoreDoesNotFightCarnivoreOnLeft() {
-		Carnivore carnivore = new Carnivore(1, false);
-		Carnivore carnivore2 = new Carnivore(3, false);
-		Board.Organisms[1][1] = carnivore;
-		Board.Organisms[1][2] = carnivore2;
-		Board.Organisms[1][3] = null;
-		Board.fight(1,2);
-		assertEquals(3, Board.Organisms[1][1].life);
+		assertEquals(false, organisms[1][2].isHasActed());
 	}
 	
 	@Test
 	public void carnivoreDoesNotFightCarnivoreOnTop() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Carnivore carnivore2 = new Carnivore(3, false);
-		Board.Organisms[0][2] = carnivore;
-		Board.Organisms[1][2] = carnivore2;
-		Board.Organisms[1][3] = null;
+		Herbivore herbivore = new Herbivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][2] = carnivore;
+		organisms[1][2] = carnivore2;
+		organisms[1][3] = herbivore;
 		Board.fight(1,2);
-		assertEquals(3, Board.Organisms[1][1].life);
+		assertEquals(carnivore, organisms[0][2]);
 	}
 	
 	@Test
 	public void carnivoreDoesNotFightCarnivoreOnBottom() {
 		Carnivore carnivore = new Carnivore(1, false);
 		Carnivore carnivore2 = new Carnivore(3, false);
-		Board.Organisms[2][2] = carnivore;
-		Board.Organisms[1][2] = carnivore2;
-		Board.Organisms[1][3] = null;
+		Herbivore herbivore = new Herbivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[2][2] = carnivore;
+		organisms[1][2] = carnivore2;
+		organisms[1][3] = herbivore;
 		Board.fight(1,2);
-		assertEquals(3, Board.Organisms[1][1].life);
+		assertEquals(carnivore2, organisms[1][2]);
 	}
 	
 	@Test
@@ -221,56 +146,90 @@ public class BoardTest {
 		Carnivore carnivore = new Carnivore(1, false);
 		Herbivore herbivore = new Herbivore(3, false);
 		Herbivore herbivore2 = new Herbivore(3, false);
-		Board.getOrganisms();
-		organisms.Organisms[0][0] = carnivore;
-		Board.organisms[0][1] = herbivore;
-		Board.organisms[0][2] = herbivore2;
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = herbivore;
+		organisms[0][2] = herbivore2;
 		Board.nextDay();
-		assertEquals(herbivore, Board.Organisms[0][1].life);
+		assertEquals(herbivore, organisms[0][1]);
 	}
 	
 	@Test
 	public void whenCarnivoresFightStrongestWins() {
-
+		Carnivore carnivore = new Carnivore(1, false);
+		Carnivore carnivore2 = new Carnivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = carnivore2;
+		Board.fight(0,0);
+		assertEquals(carnivore2, organisms[0][1]);
 	}
 	
 	@Test
 	public void whenCarnivoresFightWeakestLoses() {
-
+		Carnivore carnivore = new Carnivore(1, false);
+		Carnivore carnivore2 = new Carnivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = carnivore2;
+		Board.fight(0,0);
+		assertEquals(null, organisms[0][0]);
 	}
 	
 	@Test
 	public void whenCarnivoreWinsFightLevelRaisesWithLevelOther() {
-
-	}
-	
-	@Test
-	public void losingCarnivoreGetsEliminated() {
-
+		Carnivore carnivore = new Carnivore(1, false);
+		Carnivore carnivore2 = new Carnivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = carnivore2;
+		Board.fight(0,0);
+		assertEquals(4, carnivore2.getLife());
 	}
 	
 	@Test
 	public void carnivoresWithEqualStrengthDoNotFight() {
-
+		Carnivore carnivore = new Carnivore(3, false);
+		Carnivore carnivore2 = new Carnivore(3, false);
+		Plant plant = new Plant(1, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = carnivore2;
+		organisms[0][2] = plant;
+		Board.fight(0,1);
+		assertEquals(3, carnivore.getLife());
 	}
 	
 	@Test
 	public void carnivoreMovesWhenRightIsEmpty() {
-
+		Carnivore carnivore = new Carnivore(3, false);
+		Plant plant = new Plant(1, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = null;
+		organisms[1][0] = plant;
+		Board.nextDay();
+		assertEquals(carnivore, organisms[0][1]);
 	}
 	
 	@Test
-	public void carnivoreDoesNotMoveWhenEqualCarnivoreOnRight() {
-
+	public void carnivoreDoesNotMoveWhenPlantOnRight() {
+		Carnivore carnivore = new Carnivore(3, false);
+		Plant plant = new Plant(1, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		organisms[0][1] = plant;
+		Board.nextDay();
+		assertEquals(carnivore, organisms[0][1]);
+		
 	}
 	
 	@Test
 	public void whenAnimalActsThenHasActedIsTrue() {
-
-	}
-	
-	@Test
-	public void whenHasActedIsTrueNoActionOccurs() {
-
+		Carnivore carnivore = new Carnivore(3, false);
+		Organism [][]organisms = Board.getOrganisms();
+		organisms[0][0] = carnivore;
+		Board.nextDay();
+		assertEquals(true, carnivore.isHasActed());
 	}
 }
