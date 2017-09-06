069ce7dd56ee682c7795f8774c17c2408b7f434c
Nolan <nolan444@hotmail.com>
2017-09-06 15:57:37 +0200

    Update BoardServlet

--- src/main/java/be/vdab/web/servlets/BoardServlet.java
+++ src/main/java/be/vdab/web/servlets/BoardServlet.java
@@ -3,7 +3,6 @@ package be.vdab.web.servlets;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,59 +17,55 @@ import be.vdab.entities.Herbivore;
 import be.vdab.entities.Organism;
 import be.vdab.entities.Plant;
 import be.vdab.terrarium.Board;
-import be.vdab.terrarium.BoardException;
 import be.vdab.terrarium.util.SpecifiedAmountsTerrariumGenerator;
 import be.vdab.terrarium.util.TerrariumRenderer;
 
-/**
- * Servlet implementation class BoardServlet
- */
 @WebServlet("/board.htm")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/jsp/board.jsp";
 
     public BoardServlet() {
 
     }
-
-
+    
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}
-	protected void doGetNewBoard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		SpecifiedAmountsTerrariumGenerator generator = new SpecifiedAmountsTerrariumGenerator();
-		HttpSession session = request.getSession(false);
-				//if (session != null) {
-		generator.setSize(6, 6);
-		generator.setAmountForType(Plant.class, 2);
-		generator.setAmountForType(Herbivore.class, 3);
-		generator.setAmountForType(Carnivore.class, 2);
-		generator.setLifeForceRangeForType(Plant.class, 1, 1);
-		generator.setLifeForceRangeForType(Herbivore.class, 0, 10);
-		generator.setLifeForceRangeForType(Carnivore.class, 0, 10);
-		
-		Board board = new Board();
-		
-		board.setOrganisms(generator.generateTerrarium());
-		board.setAantalPlantenPerBeurt(1);
-
-		TerrariumRenderer renderer = new TerrariumRenderer(board.getOrganisms());
-		
-		Organism[][] organisms = board.getOrganisms();
-		
-		Map <Integer, Organism> organismsMap = new LinkedHashMap <>();
-		
-		for (int i = 0; i < board.getRow(); i++) {
-			for (int j = 0; j < board.getRow(); j++) {
-				organismsMap.put( (i * 10) + j, organisms[i][j]);
-			}
+		HttpSession session = request.getSession();
+			generator.setSize(6, 6);
+			generator.setAmountForType(Plant.class, 2);
+			generator.setAmountForType(Herbivore.class, 3);
+			generator.setAmountForType(Carnivore.class, 2);
+			generator.setLifeForceRangeForType(Plant.class, 1, 1);
+			generator.setLifeForceRangeForType(Herbivore.class, 0, 10);
+			generator.setLifeForceRangeForType(Carnivore.class, 0, 10);
 			
-		}
+			Board board = new Board();
+			
+			board.setOrganisms(generator.generateTerrarium());
+			board.setAantalPlantenPerBeurt(1);
+	
+			@SuppressWarnings("unused")
+			TerrariumRenderer renderer = new TerrariumRenderer(board.getOrganisms());
+			
+			Organism[][] organisms = board.getOrganisms();
+			
+			Map <Integer, Organism> organismsMap = new LinkedHashMap <>();
+			
+			for (int i = 0; i < board.getRow(); i++) {
+				for (int j = 0; j < board.getRow(); j++) {
+					organismsMap.put( (i * 10) + j, organisms[i][j]);
+				}
+				
+			}
 		request.setAttribute("organisms", organismsMap);
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}
+//    @Override
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//	}
 
 }
