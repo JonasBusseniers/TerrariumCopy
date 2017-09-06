4ec77b8b0e7e945a50311c093ce06da7eb122ace
Jonas <j_busseniers@hotmail.com>
2017-09-06 09:28:01 +0200

    Only 1 new plant every day

--- src/main/java/be/vdab/terrarium/Gui.java
+++ src/main/java/be/vdab/terrarium/Gui.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import be.vdab.entities.Carnivore;
 import be.vdab.entities.Herbivore;
 import be.vdab.entities.Plant;
-import be.vdab.terrarium.util.PropertiesFileTerrariumGenerator;
 import be.vdab.terrarium.util.SpecifiedAmountsTerrariumGenerator;
 import be.vdab.terrarium.util.TerrariumRenderer;
 
@@ -14,16 +13,18 @@ public class Gui {
 	public static void main(String[] args) {
 
 		SpecifiedAmountsTerrariumGenerator generator = new SpecifiedAmountsTerrariumGenerator();
-		generator.setSize(6,6);
-		generator.setAmountForType(Plant.class,2);
-		generator.setAmountForType(Herbivore.class,3);
+		generator.setSize(6, 6);
+		generator.setAmountForType(Plant.class, 2);
+		generator.setAmountForType(Herbivore.class, 3);
 		generator.setAmountForType(Carnivore.class, 2);
-		generator.setLifeForceRangeForType(Plant.class, 1,1);
-		generator.setLifeForceRangeForType(Herbivore.class,0,10);
-		generator.setLifeForceRangeForType(Carnivore.class,0,10);
+		generator.setLifeForceRangeForType(Plant.class, 1, 1);
+		generator.setLifeForceRangeForType(Herbivore.class, 0, 10);
+		generator.setLifeForceRangeForType(Carnivore.class, 0, 10);
 
-		//Board.setOrganisms(new PropertiesFileTerrariumGenerator("terrariumGame.properties").generateTerrarium());
+		// Board.setOrganisms(new
+		// PropertiesFileTerrariumGenerator("terrariumGame.properties").generateTerrarium());
 		Board.setOrganisms(generator.generateTerrarium());
+		Board.setAANTALPLANTENPERBEURT(1);
 
 		TerrariumRenderer renderer = new TerrariumRenderer(Board.getOrganisms());
 
