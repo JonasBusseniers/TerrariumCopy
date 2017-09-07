8f1e79c36daf52bfdeee1242933eba3b2ee002dc
Nolan <nolan444@hotmail.com>
2017-09-07 10:00:18 +0200

    Update Class: Dirt

--- src/main/java/be/vdab/entities/Dirt.java
+++ src/main/java/be/vdab/entities/Dirt.java
@@ -1,5 +1,13 @@
 package be.vdab.entities;
 
-public class Dirt {
+public class Dirt extends Organism {
+	
 	public String url = "images/dirt.png";
+	
+	public Dirt(int life, boolean hasActed) {
+		super(life, hasActed);
+		// TODO Auto-generated constructor stub
+	}
+
+
 }
