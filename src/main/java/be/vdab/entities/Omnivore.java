ba17050377d3248f83458a9a55bb5c102a151377
Steven <steven.gryp@outlook.com>
2017-09-08 09:28:00 +0200

    Omnivore class

---
+++ src/main/java/be/vdab/entities/Omnivore.java
@@ -0,0 +1,23 @@
+package be.vdab.entities;
+
+public class Omnivore extends Organism {
+	
+	public String url = "images/carnivore.png";
+	
+	public Omnivore (int life, boolean hasActed){
+        super(life, hasActed);
+	}
+	
+	@Override
+    public String toString() {
+        return "Omni";
+    }
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
